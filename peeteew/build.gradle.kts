2222747d0bdc239e11b6ad27366a818b00a43fe6
Tom Vercauteren <tomvercaut@gmail.com>
2024-07-05 23:58:07 +0200

    Update dependencies, Kotlin plugin, and Gradle version
    Upgraded Kotlin Gradle plugin to 2.0.0 and Gradle to 8.8. Added several new dependencies to the gradle/libs.versions.toml file and refactored the build scripts to utilize the defined versions. Removed redundant dependency constraints and cleaned up autogenerated comments from build logic files.

--- peeteew/build.gradle.kts
+++ peeteew/build.gradle.kts
@@ -3,12 +3,17 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations")
-    implementation("org.dom4j:dom4j")
-    implementation("com.fasterxml.jackson.core:jackson-core")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
-    implementation("ch.qos.logback:logback-classic")
-    implementation("ch.qos.logback:logback-core")
-    implementation("org.slf4j:slf4j-api")
+    implementation(libs.jetbrains.annotations)
+    implementation(libs.dom4j)
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.dataformat)
+    implementation(libs.jackson.datatype)
+    implementation(libs.logback.classic)
+    implementation(libs.logback.core)
+    implementation(libs.slf4j.api)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    testCompileOnly(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
 }
