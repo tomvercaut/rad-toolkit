39ff24be1a60d8a3953a67d023ebb4bed00ee2e9
Tom Vercauteren <tomvercaut@gmail.com>
2024-07-06 00:19:06 +0200

    Switch build system from Maven to Gradle.
    Replaced Maven build files with Gradle build configurations. This change includes the addition of Gradle wrapper files, build scripts, and property configurations, facilitating a more flexible and efficient build process.

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    // Include 'plugins build' to define convention plugins.
+    includeBuild("build-logic")
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "rad-toolkit"
+include("peeteew")
